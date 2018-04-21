78fdf60c459820bebea29eb80b8622e06cf0427a
¨KrisKorrel¨ <Kristian.korrel@student.uva.nl>
2018-04-21 16:31:33 +0200

    Add soft attention like action scoring
    Instead of providing the maximum number of possible actions/attentions to the understander, we now concatenate each decoder state with each encoder state through a MLP to get the action score/probability

--- seq2seq/models/teacher.py
+++ seq2seq/models/teacher.py
@@ -30,7 +30,7 @@ class Teacher(nn.Module):
         saved_log_probs (list): Description
     """
 
-    def __init__(self, input_vocab_size, embedding_dim, hidden_dim, output_vocab_size, gamma):
+    def __init__(self, input_vocab_size, embedding_dim, hidden_dim, gamma):
         """
         Summary
 
@@ -38,7 +38,6 @@ class Teacher(nn.Module):
             input_vocab_size (TYPE): Description
             embedding_dim (TYPE): Description
             hidden_dim (TYPE): Description
-            output_vocab_size (TYPE): Description
             gamma (TYPE): Description
         """
         super(Teacher, self).__init__()
@@ -51,8 +50,6 @@ class Teacher(nn.Module):
             hidden_dim=hidden_dim)
 
         self.decoder = TeacherDecoder(
-            output_vocab_size=output_vocab_size,
-            embedding_dim=0,
             hidden_dim=hidden_dim)
 
         self.saved_log_probs = []
@@ -68,8 +65,8 @@ class Teacher(nn.Module):
         Returns:
             TYPE: Description
         """
-        encoded = self.encoder(input_variable=state)
-        action_probs = self.decoder(hidden=encoded, output_length=max_len)
+        encoder_outputs, encoded_hidden = self.encoder(input_variable=state)
+        action_probs = self.decoder(encoder_outputs=encoder_outputs, hidden=encoded_hidden, output_length=max_len)
 
         return action_probs
 
@@ -86,15 +83,7 @@ class Teacher(nn.Module):
         enc_len = state.size(1)
         probabilities = self.forward(state, max_len)
 
-        # We set the probability of non-valid attentions to zero. We don't need to re-normalize as this is already done in Categorical
-        # probabilities_current_step[0, enc_len:] = 0
-        unnormalized_probs = probabilities
-        probabilities = unnormalized_probs.clone()
-        probabilities[:, :, enc_len:] = 0
-
-        # print "new"
-        # print scores[0, :, :3]
-        # print probabilities[0, :, :enc_len]
+        # print probabilities
 
         import random
         sample = random.random()
@@ -116,6 +105,8 @@ class Teacher(nn.Module):
 
             self.saved_log_probs.append(categorical_distribution.log_prob(action))
             actions.append(action.data[0])
+
+        # print actions
 
         return actions
 
@@ -196,25 +187,26 @@ class TeacherEncoder(nn.Module):
         hidden0 = self.init_hidden()
         out, hidden = self.encoder(input_embedding, hidden0)
 
-        return hidden
+        return out, hidden
 
 class TeacherDecoder(nn.Module):
-    def __init__(self, output_vocab_size, embedding_dim, hidden_dim):
+    def __init__(self, hidden_dim):
         super(TeacherDecoder, self).__init__()
 
-        self.output_vocab_size = output_vocab_size
-        self.embedding_dim = embedding_dim
         self.hidden_dim = hidden_dim
 
         # TODO: What should the input to the decoder be? Just the selected hidden state?
         # Or maybe just no input?
         # self.input_embedding = nn.Embedding(1, embedding_dim)
         self.decoder = nn.LSTM(1, hidden_dim, batch_first=True)
-        self.linear_output = nn.Linear(hidden_dim, output_vocab_size)
+
+        self.combine_enc_seq_layer = nn.Linear(hidden_dim * 2, hidden_dim)
+        self.activation = nn.ReLU()
+        self.output_layer = nn.Linear(hidden_dim, 1)
 
     # For evaluation we need to generate an attention vector for all 50 outputs while we have only 3 inputs,
     # Thats why we use max_len. Should be fixed
-    def forward(self, hidden, output_length):
+    def forward(self, encoder_outputs, hidden, output_length):
         """
         Summary
 
@@ -224,7 +216,8 @@ class TeacherDecoder(nn.Module):
         Returns:
             TYPE: Description
         """
-        action_probs_list = []
+        action_scores_list = []
+
         # TODO: We use rolled out version. If we actually won't use any (informative) input to the decoder
         # we should roll it to save computation time and have cleaner code.
         for decoder_step in range(output_length):
@@ -233,13 +226,40 @@ class TeacherDecoder(nn.Module):
 
             out, hidden = self.decoder(embedding, hidden)
 
-            action_scores = self.linear_output(out)
+            # I copied the attention mechanism from Machine's MLP attention method
+            encoder_states = encoder_outputs
+            h, c = hidden
+            decoder_states = h
+            # apply mlp to all encoder states for current decoder
+            # decoder_states --> (batch, dec_seqlen, hl_size)
+            # encoder_states --> (batch, enc_seqlen, hl_size)
+            batch_size, enc_seqlen, hl_size = encoder_states.size()
+            _, dec_seqlen, _                = decoder_states.size()
+            # (batch, enc_seqlen, hl_size) -> (batch, dec_seqlen, enc_seqlen, hl_size)
+            encoder_states_exp = encoder_states.unsqueeze(1)
+            encoder_states_exp = encoder_states_exp.expand(batch_size, dec_seqlen, enc_seqlen, hl_size)
+            # (batch, dec_seqlen, hl_size) -> (batch, dec_seqlen, enc_seqlen, hl_size)
+            decoder_states_exp = decoder_states.unsqueeze(2)
+            decoder_states_exp = decoder_states_exp.expand(batch_size, dec_seqlen, enc_seqlen, hl_size)
+            # reshape encoder and decoder states to allow batchwise computation. We will have
+            # batch_size x enc_seqlen x dec_seqlen batches. So we apply the Linear layer for each of them
+            decoder_states_tr = decoder_states_exp.contiguous().view(-1, hl_size)
+            encoder_states_tr = encoder_states_exp.contiguous().view(-1, hl_size)
+            mlp_input = torch.cat((encoder_states_tr, decoder_states_tr), dim=1)
+            # apply mlp and reshape to get in correct form
+            mlp_output = self.combine_enc_seq_layer(mlp_input)
+            mlp_output = self.activation(mlp_output)
+            out = self.output_layer(mlp_output)
+            action_scores = out.view(batch_size, dec_seqlen, enc_seqlen)
 
-            # TODO: Does it make sense to use log_softmax such that we don't have to call categorical.log_prob()?
-            # Would we then have to initialize Categorical with logits instead of probs?
-            action_probs = F.softmax(action_scores, dim=2)
-            action_probs_list.append(action_probs)
+            action_scores_list.append(action_scores)
 
-        action_probs = torch.cat(action_probs_list, dim=1)
+        # Combine the action scores for each decoder step into 1 variable
+        action_scores = torch.cat(action_scores_list, dim=1)
+
+        # For each decoder step, take the softmax over all actions to get probs
+        # TODO: Does it make sense to use log_softmax such that we don't have to call categorical.log_prob()?
+        # Would we then have to initialize Categorical with logits instead of probs?
+        action_probs = F.softmax(action_scores, dim=2)
 
         return action_probs
