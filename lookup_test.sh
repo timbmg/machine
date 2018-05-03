9f334e7b6ab4c9b12078b38129f0e6daa81bb8dd
¨KrisKorrel¨ <Kristian.korrel@student.uva.nl>
2018-05-03 14:34:30 +0200

    test script + split train into atomics and compositions

--- lookup_test.sh
+++ lookup_test.sh
@@ -1,18 +1,58 @@
 #! /bin/sh
 
-PRE_TRAIN_PATH=data/lookup-3bit/train.csv
-TRAIN_PATH=data/lookup-3bit/train.csv
-DEV_PATH=data/lookup-3bit/validation.csv
-EXPT_DIR=example
+# NOT USED
+CUDA=false
+BIDIR=false
+PONDERING=false
+USE_ATTENTION_LOSS=false
 
-# use small parameters for quicker testing
-EMB_SIZE=300
-H_SIZE=300
+TRAIN=data/lookup-3bit/train.csv
+DEV=data/lookup-3bit/validation.csv
+TEST_PATH1=data/lookup-3bit/test1_heldout.csv
+TEST_PATH2=data/lookup-3bit/test2_subset.csv
+TEST_PATH3=data/lookup-3bit/test3_hybrid.csv
+TEST_PATH4=data/lookup-3bit/test4_unseen.csv
+TEST_PATH5=data/lookup-3bit/test5_longer.csv
+OUTPUT_DIR=example
+
+EPOCHS=300
+MAX_LEN=50
+RNN_CELL='lstm'
+EMBEDDING_SIZE=300
+HIDDEN_SIZE=300
+N_LAYERS=1
+DROPOUT_P_ENCODER=0
+DROPOUT_P_DECODER=0
+TEACHER_FORCING_RATIO=0
 BATCH_SIZE=10
-EVAL_BATCH_SIZE=100
-CELL='lstm'
-EPOCH=400
-CP_EVERY=3000
-TF=0
+EVAL_BATCH_SIZE=128
+OPTIM='adam'
+LR=0.001
+SAVE_EVERY=10000000000000
+PRINT_EVERY=99999999999999999
+ATTENTION='post-rnn'
+ATTTENTION_METHOD='hard'
 
-python train_model.py --pre_train $PRE_TRAIN_PATH --train $TRAIN_PATH --dev $DEV_PATH --output_dir $EXPT_DIR --print_every 50 --embedding_size $EMB_SIZE --hidden_size $H_SIZE --rnn_cell $CELL --attention 'post-rnn' --attention_method 'hard' --epoch $EPOCH --save_every $CP_EVERY --teacher_forcing_ratio $TF --batch_size $BATCH_SIZE --eval_batch_size $EVAL_BATCH_SIZE --scale_attention_loss 1 --optim adam --lr 0.001
+echo "Start training"
+python train_model.py \
+    --train $TRAIN \
+    --pre_train $TRAIN \
+    --dev $TEST_PATH4 \
+    --output_dir $OUTPUT_DIR \
+    --epochs $EPOCHS \
+    --max_len $MAX_LEN \
+    --rnn_cell $RNN_CELL \
+    --embedding_size $EMBEDDING_SIZE \
+    --hidden_size $HIDDEN_SIZE \
+    --n_layers $N_LAYERS \
+    --dropout_p_encoder $DROPOUT_P_ENCODER \
+    --dropout_p_decoder $DROPOUT_P_DECODER \
+    --teacher_forcing_ratio $TEACHER_FORCING_RATIO \
+    --batch_size $BATCH_SIZE \
+    --eval_batch_size $EVAL_BATCH_SIZE \
+    --optim $OPTIM \
+    --lr $LR \
+    --save_every $SAVE_EVERY \
+    --print_every $PRINT_EVERY \
+    --attention $ATTENTION \
+    --attention_method $ATTTENTION_METHOD
